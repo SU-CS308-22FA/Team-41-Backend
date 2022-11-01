a52faaf347186f5de3411bce349c26b68e9e5f45
edlcn <94699354+edlcn@users.noreply.github.com>
2022-11-01 16:52:04 +0300

    The bean of mail sender added

---
+++ src/main/java/com/grove/tfb_backend/config/MailConfig.java
@@ -0,0 +1,17 @@
+package com.grove.tfb_backend.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class MailConfig {
+
+
+    @Bean
+    public JavaMailSender javaMailSender(){
+        return new JavaMailSenderImpl();
+    }
+}
--- src/main/java/com/grove/tfb_backend/mailSender/MailService.java
+++ src/main/java/com/grove/tfb_backend/mailSender/MailService.java
@@ -4,14 +4,22 @@ package com.grove.tfb_backend.mailSender;
 
 
 import com.grove.tfb_backend.user.confirmationToken.confirmationTokenDto.ConfirmationTokenDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
+
+import javax.mail.internet.MimeMessage;
+import java.io.InputStream;
 
 @Service
 public class MailService {
 
     private final JavaMailSender mailSender;
+
+
 
     public MailService(JavaMailSender mailSender) {
         this.mailSender = mailSender;
