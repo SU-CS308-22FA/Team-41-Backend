35132ed0cb77faaeee59433588e6616c18b14599
edlcn <94699354+edlcn@users.noreply.github.com>
2022-11-01 17:13:18 +0300

    Mail bean configurations changed

--- src/main/java/com/grove/tfb_backend/config/MailConfig.java
+++ src/main/java/com/grove/tfb_backend/config/MailConfig.java
@@ -6,12 +6,27 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
+import java.util.Properties;
+
 @Configuration
 public class MailConfig {
 
 
     @Bean
-    public JavaMailSender javaMailSender(){
-        return new JavaMailSenderImpl();
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("emrhn2001@gmail.com");
+        mailSender.setPassword("gcadkccrwgkmuewf");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
     }
 }
