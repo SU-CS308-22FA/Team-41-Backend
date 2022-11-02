989ab3abd60de95f9dff78213320b94108422ae3
edlcn <94699354+edlcn@users.noreply.github.com>
2022-11-02 15:13:57 +0300

    rollback

--- src/main/java/com/grove/tfb_backend/config/SecurityConfig.java
+++ src/main/java/com/grove/tfb_backend/config/SecurityConfig.java
@@ -15,7 +15,7 @@ import org.springframework.web.filter.CorsFilter;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
+/*
     @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
@@ -33,15 +33,15 @@ public class SecurityConfig {
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
-
+*/
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
                 .cors()
                 .and()
-                .csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                //.csrf().disable()
+                //.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests()
                 .antMatchers( "/**")
                 .permitAll();
