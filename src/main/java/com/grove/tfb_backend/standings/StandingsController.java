d5c7b7278aad9e07b55a365cda6a7ba0673bfd33
edlcn <emirdlcn@gmail.com>
2022-12-13 05:40:34 +0300

    cors update

--- src/main/java/com/grove/tfb_backend/standings/StandingsController.java
+++ src/main/java/com/grove/tfb_backend/standings/StandingsController.java
@@ -2,12 +2,14 @@ package com.grove.tfb_backend.standings;
 
 
 import com.grove.tfb_backend.user.userDto.GeneralHttpResponse;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("api/v1/standings")
 public class StandingsController {
