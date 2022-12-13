c7e2c58f6a5bd4d5ff9d37804e58bf6e75ad55f0
atahf <ataollah@sabanciuniv.edu>
2022-12-13 10:22:04 +0300

    added comment package file

---
+++ src/main/java/com/grove/tfb_backend/comment/CommentDao.java
@@ -0,0 +1,10 @@
+package com.grove.tfb_backend.comment;
+
+import com.grove.tfb_backend.feedback.Feedback;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface CommentDao extends JpaRepository<Comment, Long> {
+}
